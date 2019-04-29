5c15c723b8ba446a105a7e8507b01375ec6f49e5
Alican <alican.dogan@deu.edu.tr>
2019-04-29 14:44:16 +0300

    Lab9 eklendi

---
+++ Lab09_template/Lab09/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab09
+{
+
+    [Serializable]
+    abstract class Student
+    {
+        //Sadece get methodları olacak.
+        protected string name; 
+        protected string surname;
+        protected int no;
+        public Student(string name, string surname, int no)
+        {
+            
+        }
+
+
+        public override string ToString()
+        {
+            return "Ad:___ Soyad:___ No:___ Tip:____"; 
+        }
+
+    }
+}
---
+++ Lab09_template/Lab09/University.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab09
+{
+    [Serializable]
+    class University
+    {
+        private string name;
+        private List<Student> students;
+
+     
+        public University(string name)
+        {
+           
+        }
+
+        public void AddStudent(Student s)
+        {
+            students.Add(s);
+        }
+
+        public Student SearchStudent(string name)
+        {
+            Student t = null;
+           
+            return t;
+        }
+     
+        
+        public Student SearchStudent(int no)
+        {
+            Student t = null;
+
+            return t;
+        }
+
+       
+    }
+}
